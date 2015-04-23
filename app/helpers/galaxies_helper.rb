6b5d2d34e6e5061c9ba57da7cc939556d92ced76
Paul Goodman <paulsaywardgoodman@gmail.com>
2015-04-23 14:03:00 -0400

    Change fonts, make map scale to size of galaxy.

--- app/helpers/galaxies_helper.rb
+++ app/helpers/galaxies_helper.rb
@@ -81,13 +81,11 @@ module GalaxiesHelper
     end
   end
 
-  SCREEN_WIDTH = 1028
-  SCREEN_HEIGHT = 720
-
-  def convert_coords(ring, angle)
+  def convert_coords(ring, angle, size)
+    side_length = 200 * (size + 1)
     complex = Complex.polar(ring, angle*2*Math::PI)
-    x = complex.real * 100 + SCREEN_WIDTH/2
-    y = complex.imag * 100 + SCREEN_HEIGHT/2
+    x = complex.real * 100 + side_length/2
+    y = complex.imag * 100 + side_length/2
     [x,y]
   end
 
