6826559707c984d140138685234c6201f0213333
Paul Goodman <paulsaywardgoodman@gmail.com>
2015-04-25 13:52:33 -0400

    Fix bug from last bugfix

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -13,7 +13,7 @@ class SystemsController < ApplicationController
     @system = System.find(params[:id])
     if @system.update(system_params)
       @system.units.each do |unit|
-        unless current_user == @system.gm ||
+        unless current_user == @system.galaxy.gm ||
             current_user == unit.faction.user &&
             @system.links.where(destination_system: target).count > 0
           unit.destination == @system
