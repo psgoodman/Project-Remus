e27b459badcc33a81cd56702c9c930c0da2bfa48
Paul Goodman <paulsaywardgoodman@gmail.com>
2015-05-06 16:30:23 -0400

    New and create implemented.

--- app/controllers/unit_classes_controller.rb
+++ app/controllers/unit_classes_controller.rb
@@ -3,4 +3,23 @@ class UnitClassesController < ApplicationController
     @unit_class = UnitClass.new
     @faction = Faction.find(params[:faction_id])
   end
+
+  def create
+    @faction = Faction.find(params[:faction_id])
+    @unit_class = UnitClass.new(unit_class_params)
+    @unit_class.faction = @faction
+    if @unit_class.save
+      redirect_to galaxy_faction_path(@faction.galaxy, @faction)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def unit_class_params
+    params.require(:unit_class).permit(
+      :name, :description
+      )
+  end
 end
